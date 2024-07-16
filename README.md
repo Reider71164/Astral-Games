fe9bbc62f8c567f24a4066bcafcbf822babdf042
Reider71164 <91398971+Reider71164@users.noreply.github.com>
2024-07-16 03:02:06 -0700

    lots of new games

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Astral-Games
 
-wow u reely skidding look at u, go make ur own code
+this was all made by some middle schooler
+feel free to go ahead and take any games for your own site since i do not own them, neither do the sites i got them from
+dont embed my links pls
---
+++ storage/gxmes/1v1lol/js/unityUrls.js
@@ -0,0 +1,9 @@
+window.loadedUrls = true;
+
+function getGameJsonUrl() {
+	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/89/c3e9643c8f5f416582ab4a3c0193b709/WebGL.json"; //%gameJsonUrl
+}
+
+function getGameLoaderUrl() {
+	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/89/c3e9643c8f5f416582ab4a3c0193b709/UnityLoader.js"; //%gameLoaderUrl
+}
