a1751e873839805c65837d21bf021c126b53c27f
Reider71164 <91398971+Reider71164@users.noreply.github.com>
2024-07-17 17:03:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-(async() => {
+(async () => {
+  try {
     await import('./index.mjs');
-  })();
+  } catch (err) {
+    console.error('Error:', err);
+    process.exit(1); // Exit with non-zero code to indicate failure
+  }
+})();
