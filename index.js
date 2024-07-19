88112b3a454afb631150784600f549aafb03dfda
Reider71164 <91398971+Reider71164@users.noreply.github.com>
2024-07-18 19:22:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-(async () => {
-    await import('./index.mjs');
-  })();
+const http = require('http');
 
+const hostname = '0.0.0.0'; // Listen on all network interfaces
+const port = process.env.PORT || 3000; // Use the port provided by the environment or default to 3000
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
